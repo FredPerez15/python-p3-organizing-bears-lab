6ea8b665aeabf016e857032a9ebc7056a8c3e5c7
Freddy Perez <fred.perez15@gmail.com>
2023-04-07 16:37:22 +0300

    SQL bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Black', 'Mean', 1),
+('Rowdy', 9, 'F', 'Blue', 'Nice', 1),
+('Tabitha', 3, 'F', 'Black', 'Mean', 1),
+('Sergeant Brown', 19, 'M', 'Blue', 'Mean', 1),
+('Melissa', 7, 'F', 'Black', 'Nice', 1),
+('Grinch', 5, 'M', 'Black', 'Mean', 1),
+('Wendy', 1, 'F', 'Blue', 'Nice', 1),
+(NULL, 5, 'M', 'Black', 'Mean', 1);
